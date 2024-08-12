b7435cd3c7b22931e2bdb0de61f4090364c6254b
Moiseev Ilya <ilya@mois.pro>
2024-08-12 19:51:22 +0400

    chore: add links to swagger

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Get started
 1. `npm i`
-2. `npm run dev`
+2. `cp .env.example .env.local`
+3. `npm run dev`
 
 ## Create production build
 1. `npm ci`
--- constants/LINKS.ts
+++ constants/LINKS.ts
@@ -1,9 +1,21 @@
 export const LINKS = {
-    GITHUB: 'https://github.com/tonkeeper',
-    TELEGRAM_CHANNEL: 'https://t.me/tonkeeper',
-    TERMS: '#',
-    PRIVACY: '#',
-    SUPPORT: 'https://t.me/tonrostislav',
-    CONTACT: 'https://t.me/tonrostislav',
-    CONSOLE: 'https://tonconsole.com'
-}
+  GITHUB: "https://github.com/tonkeeper",
+  TELEGRAM_CHANNEL: "https://t.me/tonkeeper",
+  TERMS: "#",
+  PRIVACY: "#",
+  SUPPORT: "https://t.me/tonrostislav",
+  CONTACT: "https://t.me/tonrostislav",
+  CONSOLE: "https://tonconsole.com",
+} as const;
+
+export const SWAGGER_TAGS = {
+  BLOCKCHAIN: "Blockchain",
+  GASLESS: "Gasless",
+} as const;
+
+export const SWAGGER_LINKS = Object.keys(SWAGGER_TAGS).reduce((acc, key) => {
+  acc[key] = `${process.env.NEXT_PUBLIC_TONAPI_LANDING_PAGE_URL}/api-v2#operations-tag-${
+    SWAGGER_TAGS[key as keyof typeof SWAGGER_TAGS]
+  }`;
+  return acc;
+}, {} as { [key: string]: string });
