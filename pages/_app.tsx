d25678be2d85ab4bf241767efd05148a6e3912b1
Moiseev Ilya <ilya@mois.pro>
2024-08-23 11:57:44 +0400

    refactor: update nextra version for support mermai

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import "../styles.css";
-import "nextra-theme-docs/style.css";
-
-
-export default function Nextra({ Component, pageProps }: {Component: any, pageProps: any}) {
-    const getLayout = Component.getLayout || ((page: any) => page);
-    return getLayout(<Component {...pageProps} />);
-}
