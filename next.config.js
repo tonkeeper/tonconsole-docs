12c2a609033a4cafbd8a48a348f8e6f1338f11de
Moiseev Ilya <ilya@mois.pro>
2024-11-20 15:38:25 +0400

    chore: fixed redirect from removed page to placeholders

--- next.config.js
+++ next.config.js
@@ -8,16 +8,4 @@ module.exports = withNextra({
   images: {
     unoptimized: true,
   },
-  redirects: () => [
-    {
-      source: '/tonapi/api-v2',
-      destination: '/tonapi/rest-api',
-      permanent: true,
-    },
-    {
-      source: '/tonapi/recipes',
-      destination: '/tonapi/cookbook',
-      permanent: true,
-    },
-  ],
 });
